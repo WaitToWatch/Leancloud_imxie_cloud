c3150f57aa0fbb949010dcc960a70450f6693747
xcc3641 <446022570@qq.com>
2016-08-12 10:44:36 +0800

    fix

--- cloud_check_save_proxy.py
+++ cloud_check_save_proxy.py
@@ -1,18 +1,17 @@
 # coding: utf-8
+import time
+
 import model
 import proxy_chicken
 
 
 def check_save_proxy():
-    # 保持我的池子活着 5 个代理
-    if len(model.query_proxy()) < 5:
-        print '========寻找代理启动=========='
-        proxy_chicken.pool_load()
-        # can_be_use = proxy_chicken.get_list()
-        # print '*********%d' % len(can_be_use)
-        # for value in can_be_use:
-        #     model.save_proxy(value)
-        print '===========存储完毕==========='
+    print time.strftime('%Y-%m-%d (%H:%M)', time.localtime(time.time()))
+    # # 保持我的池子活着 5 个代理
+    # if len(model.query_proxy()) < 5:
+    print '========寻找代理启动=========='
+    proxy_chicken.pool_load()
+    print '===========存储完毕==========='
     # check_delete_proxy()
 
 
